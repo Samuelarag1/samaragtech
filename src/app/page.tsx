97afa65841c6201d1c122ccf8e138ab400a6b6d5
Samuel <samuelaragon616@gmail.com>
2025-01-05 19:17:26 -0300

    update navitgation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Header from "@/components/Header";
+import Navigation from "@/components/Navigation/Navigation";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Navigation />
+      <Header />
+    </>
+  );
 }
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,131 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+const Navbar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [startX, setStartX] = useState<number | null>(null);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleSmoothScroll = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute("href")?.substring(1);
+    const targetElement = document.getElementById(targetId || "");
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth",
+      });
+    }
+    setMenuOpen(false);
+  };
+
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchEnd = (e: React.TouchEvent) => {
+    if (startX === null) return;
+    const endX = e.changedTouches[0].clientX;
+    const diffX = endX - startX;
+
+    if (diffX > 50) {
+      setMenuOpen(true);
+    }
+    if (diffX < -50) {
+      setMenuOpen(false);
+    }
+
+    setStartX(null);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) setMenuOpen(false);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div
+      className="navbar"
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+    >
+      <div className="hamburger">
+        <input
+          className="checkbox"
+          type="checkbox"
+          checked={menuOpen}
+          onChange={toggleMenu}
+        />
+        <svg fill="none" viewBox="0 0 50 50" height="50" width="50">
+          <path
+            className="lineTop line"
+            strokeLinecap="round"
+            strokeWidth="4"
+            stroke="black"
+            d="M6 11L44 11"
+          ></path>
+          <path
+            strokeLinecap="round"
+            strokeWidth="4"
+            stroke="black"
+            d="M6 24H43"
+            className="lineMid line"
+          ></path>
+          <path
+            strokeLinecap="round"
+            strokeWidth="4"
+            stroke="black"
+            d="M6 37H43"
+            className="lineBottom line"
+          ></path>
+        </svg>
+      </div>
+
+      <nav className={`menu ${menuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <a href="#home" className="nav-link" onClick={handleSmoothScroll}>
+              Inicio
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="nav-link" onClick={handleSmoothScroll}>
+              Acerca de
+            </a>
+          </li>
+          <li>
+            <a
+              href="#services"
+              className="nav-link"
+              onClick={handleSmoothScroll}
+            >
+              Servicios
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              className="nav-link"
+              onClick={handleSmoothScroll}
+            >
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
