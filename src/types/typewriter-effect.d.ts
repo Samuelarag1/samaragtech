d7bff34e8f5f329886e9b5d2894625327b60d446
Samuel <samuelaragon616@gmail.com>
2025-01-07 23:13:48 -0300

    build done

--- src/types/typewriter-effect.d.ts
+++ src/types/typewriter-effect.d.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "typewriter-effect/dist/core" {
   const Typewriter: any;
   export default Typewriter;
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
