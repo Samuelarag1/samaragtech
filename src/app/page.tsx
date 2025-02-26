8427484a99fcefc258d65e6c37f70c00b9aef5f2
Samuel <samuelaragon616@gmail.com>
2025-02-26 14:50:35 -0300

    refactor: move Header component to a new directory and update import paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import Header from "@/components/Header/Header";
 import Navigation from "@/components/Navigation/Navigation";
 import Services from "@/components/Services/Services";
 import Projects from "@/components/Projects/Projects";
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { BsArrowRight } from "react-icons/bs";
 import { FaCrown } from "react-icons/fa6";
-import BenefitsSection from "./Benefits/Benefits";
+import BenefitsSection from "@/components/Benefits/Benefits";
 
 const Header: React.FC = () => {
   return (
